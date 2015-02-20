4ef9423cf0bd7a87e1a836caef4aba77a16673a3
Christoph Ruggenthaler <christoph.ruggenthaler@gmail.com>
2015-02-20 13:19:12 +0100

    updated parser with manual url fetching

--- src/main/java/at/ac/ait/ubicity/rss/impl/RssParser.java
+++ src/main/java/at/ac/ait/ubicity/rss/impl/RssParser.java
@@ -1,14 +1,17 @@
 package at.ac.ait.ubicity.rss.impl;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.stream.XMLInputFactory;
+
 import org.apache.log4j.Logger;
 import org.jdom2.Element;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import org.xml.sax.InputSource;
 
 import at.ac.ait.ubicity.contracts.rss.RssDTO;
 
@@ -16,7 +19,6 @@ import com.rometools.rome.feed.synd.SyndCategory;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.XmlReader;
 
 public class RssParser {
 
@@ -25,6 +27,8 @@ public class RssParser {
 	private final URL url;
 
 	private final String lastGuid;
+
+	private final XMLInputFactory factory = XMLInputFactory.newInstance();
 
 	public RssParser(String urlString, String lastGuid)
 			throws MalformedURLException {
@@ -37,56 +41,43 @@ public class RssParser {
 
 		List<RssDTO> list = new ArrayList<RssDTO>();
 
-		XmlReader reader = null;
-
 		try {
-			URLConnection conn = url.openConnection();
-			conn.setRequestProperty("User-Agent",
-					"Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0");
-
-			reader = new XmlReader(conn);
-			SyndFeed feed = new SyndFeedInput().build(reader);
+			InputSource is = new InputSource(url.openStream());
+			SyndFeed feed = new SyndFeedInput().build(is);
 
 			for (SyndEntry e : feed.getEntries()) {
-				if (isNewEntry(e.getUri())) {
-					RssDTO dto = new RssDTO();
-					dto.setId(e.getUri());
-					dto.setTitle(e.getTitle());
-					dto.setText(e.getDescription().getValue());
-					dto.setSource(e.getLink());
-					dto.setCreatedAt(e.getPublishedDate());
+				RssDTO dto = new RssDTO();
+				dto.setId(e.getUri());
+				dto.setTitle(e.getTitle());
+				dto.setText(Jsoup.clean(e.getDescription().getValue(),
+						Whitelist.simpleText()));
+				dto.setSource(e.getLink());
+				dto.setPublishedAt(e.getPublishedDate());
+				dto.setAuthor(e.getAuthor());
 
-					List<String> cats = new ArrayList<String>();
-					for (SyndCategory cat : e.getCategories()) {
-						cats.add(cat.getName());
-					}
-
-					dto.setCategories(cats);
-
-					dto.setLang(readForeignMarkup(e.getForeignMarkup(),
-							ForeignRssTag.LANG));
-
-					String geo = readForeignMarkup(e.getForeignMarkup(),
-							ForeignRssTag.GEO_POINT);
-
-					if (geo != null) {
-						String[] geoAr = geo.split(" ");
-						dto.setGeoRssPoint(Float.parseFloat(geoAr[1]),
-								Float.parseFloat(geoAr[0]));
-					}
-
-					list.add(dto);
+				List<String> cats = new ArrayList<String>();
+				for (SyndCategory cat : e.getCategories()) {
+					cats.add(cat.getName());
 				}
+
+				dto.setCategories(cats);
+
+				dto.setLang(readForeignMarkup(e.getForeignMarkup(),
+						ForeignRssTag.LANG));
+
+				String geo = readForeignMarkup(e.getForeignMarkup(),
+						ForeignRssTag.GEO_POINT);
+
+				if (geo != null) {
+					String[] geoAr = geo.split(" ");
+					dto.setGeoRssPoint(Float.parseFloat(geoAr[1]),
+							Float.parseFloat(geoAr[0]));
+				}
+
+				list.add(dto);
 			}
 
 		} catch (Exception e) {
-			if (reader != null)
-				try {
-					reader.close();
-				} catch (IOException e1) {
-					logger.warn("Exc caught while closing reader", e1);
-				}
-
 			logger.warn("Exc caught while loading entries", e);
 		}
 
@@ -94,14 +85,6 @@ public class RssParser {
 				+ this.url.toString());
 
 		return list;
-	}
-
-	private boolean isNewEntry(String curGuid) {
-
-		if (this.lastGuid == null || curGuid == null) {
-			return true;
-		}
-		return this.lastGuid.equals(curGuid);
 	}
 
 	private String readForeignMarkup(List<Element> list, ForeignRssTag tag) {
